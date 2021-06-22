cddc74345a3ebbcddd5012d30b1c5aaa3f01a100
valexent <47042660+valexent@users.noreply.github.com>
2021-06-22 16:11:43 +0200

    cleanup

---
+++ Assets/Script/TriggerTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class TriggerTest : MonoBehaviour
+{
+    public bool cardPlaced = false;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(cardPlaced == false)
+        cardPlaced = true;
+        TriggerManager.instance.UpdateCardList();
+        
+    }
+
+   
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(cardPlaced == true)
+
+        if(other.gameObject.GetComponent<TrackableBehaviour>().CurrentStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            cardPlaced = true;
+        }
+
+        cardPlaced = false;
+    }
+}
