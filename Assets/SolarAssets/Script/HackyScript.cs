58cb5728558e8a198178b2dd91eda1c103ed4aef
valexent <47042660+valexent@users.noreply.github.com>
2021-07-19 18:04:41 +0200

    started cheating script for gameplay vid

---
+++ Assets/SolarAssets/Script/HackyScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HackyScript : MonoBehaviour
+{
+    [SerializeField]
+    public List<Card> cardsToPlay;
+    [SerializeField]
+    public List<Vector2Int> positions;
+
+    int pressCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            CardManager.instance.PlaceCard(positions[pressCounter].x, positions[pressCounter].y, cardsToPlay[pressCounter]);
+            pressCounter++;
+        }
+    }
+}
